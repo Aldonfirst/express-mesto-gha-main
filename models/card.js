62363bd1e5b366574611ea766a37fdedb7a7288d
Aldonfirst <alchevck44@bk.ru>
2023-07-27 16:39:07 +0300

    test 2

--- models/card.js
+++ models/card.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const { validationMessage } = require('../utils/errorMessage');
 
-const URL_REGEX = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
-
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -13,10 +11,6 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: [true, validationMessage.required],
-    validate: {
-      validator: (url) => URL_REGEX.isUrl(url),
-      message: ' Тут должна быть URL ссылка  изображения ',
-    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const { validationMessage } = require('../utils/errorMessage');
 
-const URL_REGEX = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
-
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -19,10 +17,6 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: [true, validationMessage.required],
-    validate: {
-      validator: (url) => URL_REGEX.isUrl(url),
-      message: 'Тут должна быть URL ссылка  изображения',
-    },
   },
 }, { versionKey: false });
 
