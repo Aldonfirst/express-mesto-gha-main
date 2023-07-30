bba78c6a36ca3d1ec3245e98cfaba084e3cbbf10
Aldonfirst <alchevck44@bk.ru>
2023-07-30 15:12:46 +0300

    test2

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-const { PORT, DB_URL } = require('./utils/constants');
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,9 @@
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+// const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
-const URL_REGEX = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+// const URL_REGEX = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
 
-module.exports = {
-  PORT,
-  DB_URL,
-  URL_REGEX,
-};
+// module.exports = {
+//   PORT,
+//   DB_URL,
+//   URL_REGEX,
+// };
