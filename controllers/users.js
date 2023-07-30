c68f6670326a27e6eb045b9f6688a89ba9a43587
Aldonfirst <alchevck44@bk.ru>
2023-07-30 15:39:28 +0300

    test3

--- controllers/users.js
+++ controllers/users.js
@@ -38,7 +38,7 @@ module.exports.updateUser = (req, res) => {
         return res.status(404)
           .send({ message: 'Пользователь с указанным _id не найден.' });
       }
-      return res.send(updatedUser);
+      return res.send({ data: { name: updatedUser.name, about: updatedUser.about } });
     })
     .catch((err) => handleErrorMessage(err, res));
 };
@@ -52,7 +52,7 @@ module.exports.updateAvatar = (req, res) => {
         return res.status(404)
           .send({ message: 'Пользователь с указанным _id не найден. ' });
       }
-      return res.send(updatedUser);
+      return res.send({ data: { avatar: updatedUser.avatar } });
     })
     .catch((err) => handleErrorMessage(err, res));
 };
