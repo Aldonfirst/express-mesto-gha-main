6ba75f88cc1fbf4f04c0a1d6e103b3ccc065f0be
Aldonfirst <alchevck44@bk.ru>
2023-08-07 01:58:32 +0300

    test3

--- controllers/users.js
+++ controllers/users.js
@@ -17,9 +17,9 @@ module.exports.getUserById = (req, res, next) => {
     User.findById(userId)
       .then((user) => {
         if (!user) {
-          throw new CustomError(404);
+          throw new CustomError('Пользователь не найден', 404);
         }
-        return res.json({ data: user });
+        return res.json(user);
       })
       .catch((err) => next(err));
   }
@@ -33,7 +33,7 @@ module.exports.createUser = (req, res, next) => {
   User.create({
     name, about, avatar, email, password: hashedPassword,
   })
-    .then((user) => res.status(201).send({ data: user }))
+    .then((user) => res.status(201).send(user))
     .catch((err) => next(err));
 };
 // авторизация
@@ -44,8 +44,13 @@ module.exports.login = (req, res, next) => {
       if (!user || !bcrypt.compareSync(password, user.password)) {
         throw new CustomError(409);
       }
-      const token = jwt.sign({ _id: user._id }, SECRET_KEY, { expiresIn: '7d' });
-      res.cookie('token', token, { httpOnly: true }).send(token);
+      const token = jwt.sign(
+        { _id: user._id },
+        SECRET_KEY,
+        { expiresIn: '7d' },
+      );
+      res.cookie('token', token, { httpOnly: true })
+        .send(token);
     })
     .catch((err) => next(err));
 };
@@ -55,7 +60,7 @@ module.exports.getUserInfo = (req, res, next) => {
   User.findById(userId)
     .then((user) => {
       if (!user) {
-        throw new CustomError(404);
+        throw new CustomError('Пользователь не найден', 404);
       }
       res.send(user);
     })
@@ -66,12 +71,7 @@ module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
   const { _id } = req.user;
   User.findByIdAndUpdate(_id, { name, about }, { new: true, runValidators: true })
-    .then((updatedUser) => {
-      if (!updatedUser) {
-        throw new CustomError(404);
-      }
-      return res.send(updatedUser);
-    })
+    .then((updatedUser) => res.send(updatedUser))
     .catch((err) => next(err));
 };
 
@@ -79,11 +79,6 @@ module.exports.updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
   const { _id } = req.user;
   User.findByIdAndUpdate(_id, { avatar }, { new: true, runValidators: true })
-    .then((updatedUser) => {
-      if (!updatedUser) {
-        throw new CustomError(404);
-      }
-      return res.send(updatedUser);
-    })
+    .then((updatedUser) => res.send(updatedUser))
     .catch((err) => next(err));
 };
